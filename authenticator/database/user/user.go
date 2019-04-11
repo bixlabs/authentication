ee979bb4919bb54a6f5227766468658d8151c39c
Joseph Arrieta <jac.wshmstr@gmail.com>
2019-04-11 18:13:16 -0500

    made the linter happy with temporal fake repo implementation

--- authenticator/database/user/user.go
+++ authenticator/database/user/user.go
@@ -3,9 +3,33 @@ package user
 import "github.com/bixlabs/authentication/authenticator/structures"
 
 type Repository interface {
-	create(user structures.User) (error, structures.User)
-	verifyPassword(password string) (error, bool)
-	changePassword(password string) error
-	saveResetPasswordToken(token string) error
-	verifyResetPasswordToken(token string) (error, bool)
+	Create(user structures.User) (error, structures.User)
+	VerifyPassword(password string) (error, bool)
+	ChangePassword(password string) error
+	SaveResetPasswordToken(token string) error
+	VerifyResetPasswordToken(token string) (error, bool)
+}
+
+//TODO: This is temporal until we have the real implementation, the linter is complaining about the interface being unused.
+type FakeRepo struct {
+}
+
+func (FakeRepo) Create(user structures.User) (error, structures.User) {
+	panic("implement me")
+}
+
+func (FakeRepo) VerifyPassword(password string) (error, bool) {
+	panic("implement me")
+}
+
+func (FakeRepo) ChangePassword(password string) error {
+	panic("implement me")
+}
+
+func (FakeRepo) SaveResetPasswordToken(token string) error {
+	panic("implement me")
+}
+
+func (FakeRepo) VerifyResetPasswordToken(token string) (error, bool) {
+	panic("implement me")
 }
