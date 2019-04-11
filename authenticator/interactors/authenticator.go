21b4c2d5302ef76c7a1bce0e159e98227b23669b
Joseph Arrieta <jac1013@users.noreply.github.com>
2019-04-11 09:57:04 -0500

    Basic business definition (#1)
    * Added simple business definition
    
    * Added simple placeholder for database implementation, model, and data mapper
    
    * changed the module name to match the project
    
    * changed the old example code to match the module name to leave it working
    
    * Added the identifier to the user business object
    
    * Go modules (#11)
    
    Migration from dep to Go module 1.11
    
    Added the CLI run version to the Makefile definition
    
    Removed documentation about the layout, added binaries to the dependencies
    
    * Api documentation (#12)
    
    * Added API documentation through swagger
    
    * Just a minor typo in the documentation
    
    * Ensured that the documentation is in place when running the project
    
    * changed the path where api is so we have to change the configation for build in MakeFile
    
    * Added the installation step running the tests
    
    * fixed the usage of dot imports and some naming, removed some comments (#16)
    
    * gofmt changes and some renaming of folders

---
+++ authenticator/interactors/authenticator.go
@@ -0,0 +1,10 @@
+package interactors
+
+import "github.com/bixlabs/authentication/authenticator/structures"
+
+type Authenticator interface {
+	Login(email, password string) (error, structures.LoginResponse)
+	Signup(user structures.User) (error, structures.LoginResponse)
+	PasswordChange(oldPassword, newPassword string) error
+	ResetPassword(email string) error
+}
---
+++ database/data-mappers/user.go
@@ -0,0 +1,16 @@
+package data_mappers
+
+import (
+	"github.com/bixlabs/authentication/authenticator/structures"
+	"github.com/bixlabs/authentication/database/model"
+)
+
+func UserToDatabaseModel(user structures.User) model.User {
+	// TODO: this needs to be implemented.
+	return model.User{}
+}
+
+func DatabaseModelToUser(user model.User) structures.User {
+	// TODO: this needs to be implemented.
+	return structures.User{}
+}
