50c1a2935ac132618ee8b77999958b7d368a757a
Joseph Arrieta <jac.wshmstr@gmail.com>
2019-07-06 17:17:44 -0500

    we install swaggo also outside of go.mod to have the binary available in the CI

--- deps.sh
+++ deps.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin v1.17.1
+go get -u github.com/swaggo/swag/cmd/swag
 make test
 
