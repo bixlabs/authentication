ae951cf834c344475d24460ada912cb012917568
Joseph Arrieta <jac1013@users.noreply.github.com>
2020-03-30 15:31:41 -0500

    Improve logging configuration (#91)
    * Improve logging configuration
    
    * Fix linter issues

--- tools/logger.go
+++ tools/logger.go
@@ -6,18 +6,35 @@ import (
 )
 
 // For more information: https://github.com/sirupsen/logrus/blob/master/example_basic_test.go
+// If we want to use a specific logging service there
+// are multiple formatters implementations here: https://github.com/sirupsen/logrus#formatters
 
 var log *logrus.Logger //nolint
 
 func InitializeLogger() {
-	log = logrus.New()
-	log.SetLevel(logrus.TraceLevel)
-
-	if os.Getenv("AUTH_SERVER_APP_ENV") == "production" {
-		log.SetLevel(logrus.InfoLevel)
+	if isProduction() {
+		initializeLoggerForProduction()
+	} else {
+		initializeLogger()
 	}
+}
 
-	log.SetOutput(os.Stdout)
+func isProduction() bool {
+	return os.Getenv("AUTH_SERVER_APP_ENV") == "production"
+}
+
+func initializeLoggerForProduction() {
+	initializeLogger()
+	log.Level = logrus.InfoLevel
+	log.SetReportCaller(false)
+}
+
+func initializeLogger() {
+	log = logrus.New()
+	log.Formatter = &logrus.TextFormatter{FullTimestamp: true}
+	log.SetReportCaller(true)
+	log.Level = logrus.TraceLevel
+	log.Out = os.Stdout
 }
 
 func Log() *logrus.Logger {
